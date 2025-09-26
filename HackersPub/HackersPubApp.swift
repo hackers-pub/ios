ffc37b341a61987ff968ed1ff005f923e1b90e46
Jihyeok Seo <ji@hyeok.org>
2025-09-26 19:49:04 +0900

    Initial Commit

---
+++ HackersPub/HackersPubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackersPubApp.swift
+//  HackersPub
+//
+//  Created by Jihyeok Seo on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HackersPubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackersPubTests/HackersPubTests.swift
@@ -0,0 +1,16 @@
+//
+//  HackersPubTests.swift
+//  HackersPubTests
+//
+//  Created by Jihyeok Seo on 9/26/25.
+//
+
+import Testing
+
+struct HackersPubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
