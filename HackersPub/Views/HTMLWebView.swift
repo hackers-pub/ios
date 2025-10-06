8a6228f750a5d4a7c308a39b8c3d067ccfa52ab0
Jihyeok Seo <ji@hyeok.org>
2025-10-06 20:45:46 +0900

    Disable pinch to zoom in webview

--- HackersPub/Views/HTMLWebView.swift
+++ HackersPub/Views/HTMLWebView.swift
@@ -55,6 +55,17 @@ struct HTMLWebView: UIViewRepresentable {
         webView.backgroundColor = .clear
         webView.navigationDelegate = context.coordinator
         webView.scrollView.isScrollEnabled = false
+
+        // Disable pinch to zoom
+        let source = """
+        var meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+        document.getElementsByTagName('head')[0].appendChild(meta);
+        """
+        let script = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        webView.configuration.userContentController.addUserScript(script)
+
         return webView
     }
 
